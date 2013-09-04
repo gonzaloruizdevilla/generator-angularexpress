b9a4153d5faf1754ad4c89e1bababf750b88513f
Gonzalo Ruiz de Villa <gonzalo.ruizdevilla@adesis.com>
2013-09-04 13:03:28 -0500

    fix(controller) JavaScript min controller gets $http injected

--- templates/javascript-min/controller.js
+++ templates/javascript-min/controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('<%= _.camelize(appname) %>App')
-  .controller('<%= _.classify(name) %>Ctrl', ['$scope', function ($scope) {
+  .controller('<%= _.classify(name) %>Ctrl', ['$scope', '$http', function ($scope, $http) {
     $http.get("/api/awesomeThings").success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
